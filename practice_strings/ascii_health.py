bb486d3ba5fca022a842e6bb3dbb1ea07ec916d8
Amelie Chambers <amelie_chambers@outlook.com>
2025-10-20 03:44:45 +0000

    horizontal&plentiful hearts!

--- practice_strings/ascii_health.py
+++ practice_strings/ascii_health.py
@@ -1,4 +1,12 @@
-heart = '__   __ \n/  \\ /  \\ \n\\       /    \n *.   .*     \n   \\ /    \n    *\n'
-# print(heart)
+# heart1 = '  _   _\n(   V   )\n \\     /\n  \\   /\n   \\ /'
 
-# try0 = heart + ' ' + heart + ' ' + heart + ' ' + heart + ' ' + heart + ' '
+# **Make into a class!!**
+line1 = "  _   _    " * 3
+line2 = "(   V   )  " * 3
+line3 = " \\     /   " * 3
+line4 = "  \\   /    " * 3
+line5 = "   \\ /     " * 3
+lines = [line1, line2, line3, line4, line5]
+
+for line in lines:
+    print(line, end="\n")
